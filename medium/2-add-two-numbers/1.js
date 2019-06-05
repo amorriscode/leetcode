bed78631f2584ef0c360bae393d9493b378c1db3
Anthony Morris <anthony@amorrissound.com>
2019-06-04 21:40:49 -0700

    Solve leetcode #2

--- medium/2-add-two-numbers/1.js
+++ medium/2-add-two-numbers/1.js
@@ -5,11 +5,79 @@
  *     this.next = null;
  * }
  */
+
+const combineLinkedList = (node) => {
+  return node.next
+    ? `${node.val}${combineLinkedList(node.next)}`
+    : node.val;
+}
+
+const linkedListToNumberArray = (list) => {
+  const combinedLinkedList = combineLinkedList(list);
+  return combinedLinkedList.length > 1
+    ? combinedLinkedList.split('')
+    : [combinedLinkedList];
+}
+
+const numberArrayToLinkedList = (numberArray) => {
+  // Turn every number into a ListNode
+  // then reduce to a single list
+  return numberArray
+    .map(number => new ListNode(number))
+    .reduce((prevNode, currNode) => {
+      if (!prevNode) {
+        return currNode;
+      }
+
+      let traverseNode = prevNode;
+
+      while (traverseNode.next) {
+        traverseNode = traverseNode.next;
+      }
+
+      traverseNode.next = currNode;
+
+      return prevNode;
+    });
+}
+
+const addNumberArraysByIndex = (numberOne, numberTwo) => {
+  let smaller = numberOne;
+  let larger = numberTwo;
+
+  if (+numberOne.join('').length > +numberTwo.join('').length) {
+    smaller = numberTwo;
+    larger = numberOne;
+  }
+
+  const addedArray = larger.map((number, index) => {
+    return smaller[index]
+      ? +number + +smaller[index]
+      : number;
+  });
+
+  addedArray.forEach((number, index) => {
+    if (number.toString().length > 1) {
+      addedArray[index + 1] = addedArray[index + 1]
+        ? +addedArray[index + 1] + 1
+        : 1;
+      addedArray[index] = number % 10;
+    }
+  });
+
+  return addedArray;
+}
+
 /**
  * @param {ListNode} l1
  * @param {ListNode} l2
  * @return {ListNode}
  */
-var addTwoNumbers = function(l1, l2) {
-    
+const addTwoNumbers = (l1, l2) => {
+  const numberOne = linkedListToNumberArray(l1);
+  const numberTwo = linkedListToNumberArray(l2);
+
+  const addedNumber = addNumberArraysByIndex(numberOne, numberTwo);
+
+  return numberArrayToLinkedList(addedNumber);
 };
