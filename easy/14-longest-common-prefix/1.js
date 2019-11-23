829eca3084ef6558870722914b88173610387666
Anthony Morris <anthony@amorrissound.com>
2019-11-23 14:10:05 -0800

    Solve longest common prefix

---
+++ easy/14-longest-common-prefix/1.js
@@ -0,0 +1,30 @@
+/**
+ * @param {string[]} strs
+ * @return {string}
+ */
+const longestCommonPrefix = strs => {
+  let longestPrefix = '';
+  let finished = false;
+  let index = 0;
+
+  let tempPrefix = '';
+  while (!finished && strs.length) {
+    if (index > strs[0].length) {
+      finished = true;
+      break;
+    } else {
+      tempPrefix += strs[0][index];
+    }
+    
+    const isCommonPrefix = strs.every(str => str.startsWith(tempPrefix));
+
+    if (isCommonPrefix) {
+      longestPrefix = tempPrefix;
+      index++;
+    } else {
+      finished = true;
+    }
+  }
+  
+  return longestPrefix;
+};
